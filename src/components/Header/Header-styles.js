b63302e7f28cea14599e8198106a86e148c7e82e
Jennifer <jennifersusan@Jennifers-MacBook-Pro.local>
2022-03-20 19:24:03 -0500

    added logo to header

--- src/components/Header/Header-styles.js
+++ src/components/Header/Header-styles.js
@@ -4,19 +4,38 @@ export const Wrapper = styled.div`
     font-family: 'Open Sans';
     background: #3AB3A8;
 
+    
+
     .header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 30px;
+       position: relative;
+        padding: 50px;
+        height: 145px;
+        
+    }
+
+    img {
+        position: absolute;
+        left: 22%;
+        top: 25%;
+        height: 48px;
+        width: 95px;
+        
     }
     h1 {
+        position: absolute;
         margin: 0;
         color: #FFFFFF;
         font-size: 64px;
+        top: 50%;
+        left: 29px;
+        
     }
 
     .links {
+        position: absolute;
+        top:50%;
+        transform: translateY(-50%);
+        right: 5%;
         display: flex;
         gap: 50px;
         font-size: 24px;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,14 @@ const Header = () => {
     return(
         <Wrapper>
         <div className="header">
+            <div className="logo">
+            <img src="/aw-logo.png" alt="logo" />
             <h1>Apply Wiz</h1>
+            
+            </div>
+            
+           
+           
             <div className='links'>
                 <Link to='/home' style={{textDecoration: 'none', color: '#FFFF'}}>HOME</Link>
                 <Link to='/profile' style={{textDecoration: 'none', color: '#FFFF'}}>PROFILE</Link>
